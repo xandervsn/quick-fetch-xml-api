51da5368d4d33be0547f49d09118e48f4079747b
Xander Siruno-Nebel <73612166+xandervsn@users.noreply.github.com>
2022-07-01 11:52:54 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const api = 'https://ws.audioscrobbler.com/2.0/?';
+const apiKey = 'KEY';
+const method = 'method=track.getInfo&'
+
+function findDuration(artist, track){
+    url = api + artist + track + method + apiKey
+    fetch(url)
+        .then(res => res.text())
+        .then(data => {
+            let parser = new DOMParser;
+            xmlDoc = parser.parseFromString(data, 'text/xml')
+            urlData = xmlDoc.getElementsByTagName('duration')
+            duration = urlData[0].firstChild.nodeValue
+            secs = duration/1000
+            seconds = secs%60
+            minutes = (secs-seconds)/60
+            console.log(minutes + ":" + seconds)
+        })
+}
+
+document.getElementById('button').onclick = function onClick(){
+    let input = document.getElementById('textField').value
+    let artist = 'artist=' + input.split(' - ')[0] + '&'
+    let track = 'track=' + input.split(' - ')[1] + '&'
+    findDuration(artist, track);
+}
